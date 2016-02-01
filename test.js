37d1fa62b1e5a2bf4a254ea23bfe6ad559cf1636
Nick Meldrum <nick@nickmeldrum.com>
2016-02-01 14:12:46 +0000

    tests ajax posting of multipart formdata

--- test.js
+++ test.js
@@ -1,4 +1,10 @@
 var ajax = require("basic-ajax");
 
-console.log(ajax);
+var content = '<a id="a"><b id="b">hey!</b></a>'; // the body of the new file...
+var blob = new Blob([content], { type: "text/xml"});
+
+var formData = new FormData();
+formData.append("testfile", blob);
+
+ajax.post('/', {}, formData);
 
